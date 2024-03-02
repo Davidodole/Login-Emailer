0f872ca5e5bc41f6b2cabb9981cd107702a01c3a
Davidodole <124046210+Davidodole@users.noreply.github.com>
2024-03-02 13:47:59 -0800

    inital commit

--- file.sql
+++ file.sql
@@ -1,4 +1,4 @@
-CREATE TABLE usersdata(
+CREATE TABLE userdatabase(
     id serial PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const LocalStrategy = require("passport-local");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const nodEmailer = require("nodemailer");
-const GoogleStrategy = require("passport-google-oauth2")
+const GoogleStrategy = require("passport-google-oauth2").Strategy
 require("dotenv").config();
 const currentDate = new Date()
 const saltRound = 10;
@@ -18,11 +18,11 @@ const app = express();
 
 // setting the database enetering 
 const db = new pg.Client({
-    user: "postgres",
-    host : "localhost",
-    database : "Users",
-    password : process.env.PASSWORD,
-    port : 5432,
+    user: process.env.PG_USER,
+    host : process.env.PG_HOST,
+    database : process.env.PG_DATABASE,
+    password : process.env.PG_PASSWORD,
+    port : process.env.PG_PORT,
 });
 
 
@@ -53,7 +53,7 @@ app.post("/register", async (req, res)=>{
 
         // checking if the user already exit 
         const userCheck = await db.query(
-        "SELECT * FROM usersdata WHERE username = $1",
+        "SELECT * FROM userdatabase WHERE username = $1",
         [email]
     );
 
@@ -64,37 +64,10 @@ app.post("/register", async (req, res)=>{
         // Hashing password and storing inside the database 
         bcrypt.hash(password, saltRound,(err, hash)=>{
             const result = db.query(
-                "INSERT INTO usersdata (username, password) VALUES($1, $2)",
+                "INSERT INTO userdatabase (username, password) VALUES($1, $2)",
                 [email, hash],(err, results)=>{
                     if(err) throw err;
                     res.redirect("/login");
-                    
-                    // setting a email transporter 
-                    const transporter = nodEmailer.createTransport({
-                    service: "gmail",
-                    host: "smtp.gmail.com",
-                    port: 587,
-                    secure: false,
-                    auth: {
-                        user: process.env.API_GMAIL,
-                        pass: process.env.API_PASSWORD,
-                    }
-                    });
-                    const mailOptions = {
-                    from: {
-                        name: "TechGenius",
-                        address: process.env.API_GMAIL,
-                    },
-                    to: email,
-                    subject: "Sending email from Nodejs Server",
-                    html: `<h3> Thank you for signing up</h3>
-                    <p>please confirm your email if probably is not correct ${email} </p>
-                    <p>This is the time you sign up : ${currentDate.toDateString()}</P>
-                    `
-                    }
-                    transporter.sendMail(mailOptions, (err, info)=>{
-                        if(err) throw err;
-                    });
                 }
             );        
         });
@@ -127,17 +100,19 @@ app.get("/secret",(req, res)=>{
     }
 });
 app.get("/auth/google/secrets", passport.authenticate("google",{
-    successRedirect: "secret",
+    successRedirect: "/secret",
     failureRedirect: "/",
 }))
 
-passport.use("local", new LocalStrategy(async(username, password, cb)=>{
+passport.use(
+    "local",
+    new LocalStrategy(async(username, password, cb)=>{
     // using try and catch method for any error 
 
     try {
                  // checking if the user already exit 
     const userChecked =await db.query( 
-         "SELECT * FROM usersdata WHERE username = $1",
+         "SELECT * FROM userdatabase WHERE username = $1",
         [username]
         );
         
@@ -147,40 +122,13 @@ passport.use("local", new LocalStrategy(async(username, password, cb)=>{
         const passwordChecker = userID.password;
         bcrypt.compare(password, passwordChecker, (err, result)=>{
             if(err){
-                return cd(err)
+                return cb(err)
             }else{
                 if(result){
-                    return cb(null, userID)
-                    // setting a email transporter 
-                    const transporter = nodEmailer.createTransport({
-                        service: "gmail",
-                        host: "smtp.gmail.com",
-                        port: 587,
-                        secure: false,
-                        auth: {
-                            user: process.env.API_GMAIL,
-                            pass: process.env.API_PASSWORD,
-                        }
-                        });
-                        const mailOptions = {
-                        from: {
-                            name: "TechGenius",
-                            address: process.env.API_GMAIL,
-                        },
-                        to: username,
-                        subject: "You've Just Login",
-                        html: `<h3>Detect New Login</h3>
-                        <p>If you didn't perform this login</p>
-                        <p>please report this to the web developer: ${process.env.API_GMAIL}</p>
-                        <p> ${currentDate.toDateString()}</P>
-                        <p> ${currentDate.getTime()}</p>
-                        `
-                        }
-                        transporter.sendMail(mailOptions, (err, info)=>{
-                            if(err) throw err;
-                        });
+                    return cb(null, result);
+                    Login(userID.username);
                 }else{
-                    return cb(null, false)
+                    return cb("user does not exits");
                 }
             }
         })
@@ -198,8 +146,22 @@ passport.use(
     callbackURL: "http://127.0.0.1:4000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
 }, async(accessToken, refreshToken, profile, cb)=>{
-    console.log(profile);
-}))
+    
+        // checking if user already exist then send to the secret page direct 
+    const userCheckedLogin = await db.query(
+        "SELECT * FROM userdatabase WHERE username = $1",
+        [profile.email]
+    );
+    if(userCheckedLogin.rows.length === 0){
+        const userID = await db.query("INSERT INTO userdatabase (username, password) VALUES($1, $2)",[profile.email, profile.id]);
+        cb(null, userID.rows[0]);
+        signUp(profile.email)
+    }else{
+        cb(null, userCheckedLogin.rows[0]);
+        Login(profile.email);
+    }
+    }
+))
 
 passport.serializeUser((user, cb)=>{
     return cb(null, user)
@@ -214,9 +176,72 @@ const PORT = process.env.PORT || 5500;
 app.listen(PORT, ()=> console.log(`http://127.0.0.1:${PORT}`));
 
 
+// anytime the user login to the website they will receive a notification message that a new logged in as been detected and also specify the time 
+
+function Login(myEmail){
+    // setting a email transporter 
+    const transporter = nodEmailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.API_GMAIL,
+            pass: process.env.API_PASSWORD,
+        }
+        });
+        const mailOptions = {
+        from: {
+            name: "TechGenius",
+            address: process.env.API_GMAIL,
+        },
+        to: myEmail,
+        subject: "You've Just Login",
+        html: `<h3>Detect New Login</h3>
+        <p>If you didn't perform this login</p>
+        <p>please report this to the web developer: ${process.env.API_GMAIL}</p>
+        <p> ${currentDate.toDateString()}</P>
+        <p> ${currentDate.getTime()}</p>
+        `
+        }
+        transporter.sendMail(mailOptions, (err, info)=>{
+            if(err) throw err;
+        });
+}
 
 
 
+// when a user sign up to the website then this email will be sent to them as notification for signing up 
+function signUp(myEmail){
+    
+                    // setting a email transporter 
+                    const transporter = nodEmailer.createTransport({
+                        service: "gmail",
+                        host: "smtp.gmail.com",
+                        port: 587,
+                        secure: false,
+                        auth: {
+                            user: process.env.API_GMAIL,
+                            pass: process.env.API_PASSWORD,
+                        }
+                        });
+                        const mailOptions = {
+                        from: {
+                            name: "TechGenius",
+                            address: process.env.API_GMAIL,
+                        },
+                        to: myEmail,
+                        subject: "Sending email from Nodejs Server",
+                        html: `<h3> Thank you for signing up</h3>
+                        <p>please confirm your email if probably is not correct ${myEmail} </p>
+                        <p>This is the time you sign up : ${currentDate.toDateString()}</P>
+                        <p> ${currentDate.getTime()}</p>
+                        `
+                        }
+                        transporter.sendMail(mailOptions, (err, info)=>{
+                            if(err) throw err;
+                        });
+}
 
 
 
